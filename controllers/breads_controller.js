04dd627da21b4fc2189691abe13efa5dfa954ab6
Jacob Garcia <jacobroxasgarcia@yahoo.com>
2022-06-06 09:55:15 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -19,9 +19,10 @@ breads.get('/:arrayIndex', (req, res) => {
         bread:Bread[req.params.arrayIndex]
       })
     } else {
-      res.send('404')
+      res.render('404')
     }
   })
+  
   
   
 
