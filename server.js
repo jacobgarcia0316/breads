df5f7f174d4b3398076b10c3c07750d0c986aa57
Jacob Garcia <jacobroxasgarcia@yahoo.com>
2022-06-06 09:34:54 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('welcome to bred')
+  res.send('welcome to bread')
 })
 
 //breads
---
+++ views/index.jsx
@@ -0,0 +1,27 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function Index ({breads}) {
+    return (
+      <Default>
+        <h2>Index Page</h2>
+        <ul>
+          {
+            breads.map((bread, index)=> {
+                return (
+                    <li key={index}>
+                    <a href={`/breads/${index}`}>
+                    {bread.name}
+                    </a>
+                    </li>
+            )
+        })
+}
+
+        </ul>
+        {/* <p> i have {breads[0].name} bread woo</p> */}
+      </Default>
+    )
+}
+
+module.exports = Index
